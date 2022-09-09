1d84f03a2ae16171c1770588df4d98f71313aec6
Inhoob <>
2022-09-10 05:47:17 +0900

    Style : GlobalStyle 설정

---
+++ client/src/assets/styles/GlobalStyle.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+  }
+  :root {
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 400;
+    --toastify-toast-width: 400px;
+
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-size: revert;
+    /* font-weight: revert; */
+  }
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  ol, ul, li {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+    &:link,
+    &:visited {
+      color: inherit;
+    }
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
