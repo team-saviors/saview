910c49c08d18af4795e83117588b561978e2abe0
yeonkkk <tjddus0302@gmail.com>
2022-09-14 15:17:38 +0900

    deploy test

---
+++ server/src/main/java/server/controller/TestController.java
@@ -0,0 +1,13 @@
+package server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testDeploy() {
+        return "Hello World!";
+    }
+}
