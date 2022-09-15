fb7d4f3e9586e98da43c6316d9bf9c5f0c81dc9d
Kyoungryun Kim <krkim1913@gmail.com>
2022-09-15 10:34:08 +0900

    Feat: Add Cors configuration

--- server/src/main/java/server/ServerApplication.java
+++ server/src/main/java/server/ServerApplication.java
@@ -2,12 +2,25 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("www.saview.link");
+            }
+        };
+    }
 
 }
