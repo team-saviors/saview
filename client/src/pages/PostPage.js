e482415ffd78bf789cefcc3d2bc547a6e224458c
Inhoob <>
2022-09-15 16:20:40 +0900

    Feat : Answers 불러오기

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
 import { useEffect } from 'react';
+import Answer from '../components/Answer';
+import { answerStore } from '../store/store';
 const PostPage = () => {
-  // async function getUser() {
-  //   try {
-  //     const response = await axios.get('/test');
-  //     console.log(response);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-  // useEffect(() => PostPage());
-  axios
-    .get(
-      'http://ec2-13-124-100-165.ap-northeast-2.compute.amazonaws.com:8080/test'
-    )
-    .then((res) => console.log(res));
-  return <>hi</>;
+  const { answers } = answerStore();
+  return (
+    <>
+      {answers.map((answer) => (
+        <Answer key={answer.answerId} answer={answer} />
+      ))}
+    </>
+  );
 };
 
 export default PostPage;
