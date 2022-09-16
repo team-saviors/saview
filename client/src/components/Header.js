9c721c776de62ae4cb261f1e3af4173683cd36af
pjhyeok <vndepddl04@naver.com>
2022-09-16 20:40:58 +0900

    [FE] Feat : header component 완성.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,50 @@
+// import { Link } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import styled from 'styled-components';
+const NavBar = styled.header`
+  /* width: 100%; */
+  height: 85px;
+  max-width: 1180px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+  margin: auto;
+
+  /* background-color: blue; */
+`;
+const LogoBox = styled(Box)`
+  /* border: 1px solid; */
+  > h1 {
+    width: 105px;
+    height: 32px;
+  }
+`;
+const Loginbox = styled(Box)``;
+const LoginButton = styled(Button)`
+  font-size: 1.125rem;
+  font-weight: 600;
+  color: black;
+  &:hover {
+    background-color: transparent;
+    box-shadow: transparent;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+`;
 const Header = () => {
   return (
-    <header
-      style={{
-        width: '100%',
-        height: '85px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'blue',
-      }}
-    >
-      헤더(layout 예시)
-    </header>
+    <NavBar>
+      <LogoBox>
+        <h1>SAVIEW</h1>
+      </LogoBox>
+      <Loginbox>
+        <LoginButton disableRipple variant="text">
+          로그인
+        </LoginButton>
+      </Loginbox>
+    </NavBar>
   );
 };
 export default Header;
