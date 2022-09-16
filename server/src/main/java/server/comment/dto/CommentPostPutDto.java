d3638f1a59b16af5c23ed1ea85409e934c2e0801
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-09-16 16:18:22 +0900

    comment CRUD 구현 (#38)
    * feat: comment CRUD

---
+++ server/src/main/java/server/comment/dto/CommentPostPutDto.java
@@ -0,0 +1,13 @@
+package server.comment.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class CommentPostPutDto {
+
+    @NotBlank(message = "내용을 입력하세요.")
+    private String content;
+
+}
