ba5a11b7746c0a34ff4180ab782e7f71815652a8
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-09-16 10:01:08 +0900

    User Get, Put, Delete  (#31)
    * Feat: user CRUD

--- server/src/main/java/server/user/dto/UserResponseDto.java
+++ server/src/main/java/server/user/dto/UserResponseDto.java
@@ -1,8 +1,10 @@
 package server.user.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class UserResponseDto {
 
     private String loginId;
