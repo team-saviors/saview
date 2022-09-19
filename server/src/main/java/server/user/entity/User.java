a61b5ca238989de2970bbf294a19bc0bfc045652
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2022-09-19 11:13:40 +0900

    Update server/src/main/java/server/user/entity/User.java

--- server/src/main/java/server/user/entity/User.java
+++ server/src/main/java/server/user/entity/User.java
@@ -21,7 +21,7 @@ public class User extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false, unique = true, length = 50)
