e746e3e2e176af3a9edbc9ebecd2ddde8af3c80a
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-09-19 19:42:16 +0900

    feat: answers 페이지네이션 적용 (#59)

--- server/src/main/java/server/answer/mapper/AnswerMapper.java
+++ server/src/main/java/server/answer/mapper/AnswerMapper.java
@@ -5,13 +5,33 @@ import server.answer.dto.AnswerPostPutDto;
 import server.answer.dto.AnswerResponseDto;
 import server.answer.entity.Answer;
 import server.comment.service.CommentService;
+import server.question.dto.QuestionsResponseDto;
+import server.question.entity.Question;
 import server.user.mapper.UserMapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Mapper(componentModel = "spring")
 public interface AnswerMapper {
 
     Answer answerPostPutDtoToAnswer(AnswerPostPutDto answerPostPutDto);
+    default List<AnswerResponseDto> AnswersToAnswersResponseDtos(List<Answer> answers,
+                                                                 UserMapper userMapper,
+                                                                 CommentService commentService) {
+        if ( answers == null ) {
+            return null;
+        }
+
+        List<AnswerResponseDto> list = new ArrayList<>(answers.size());
+        for ( Answer answer : answers ) {
+            list.add( answerToAnswerResponseDto( answer, userMapper, commentService ) );
+        }
+
+        return list;
+    }
+
 
     default AnswerResponseDto answerToAnswerResponseDto(Answer answer,
                                                         UserMapper userMapper,
--- server/src/main/java/server/user/dto/UserPutDto.java
+++ server/src/main/java/server/user/dto/UserPutDto.java
@@ -19,8 +19,7 @@ public class UserPutDto {
     @NotBlank(message = "닉네임은 반드시 입력해야합니다.")
 
     @Pattern(regexp = "(^[a-zA-Z0-9가-힣]+(\\s[a-zA-Z0-9가-힣]+)*$)",
-
-            message = "닉네임은 10자 이하의 영문으로 작성해야하며, 연속된 공백을 사용할 수 없습니다.")
+            message = "닉네임은 10자 이하의 영문자, 숫자, 한글을 사용하여 작성해야하며, 연속된 공백을 사용할 수 없습니다.")
     private String nickname;
 
     private String profile;
