bc43f461ba512d836e708e66196701cc51f2d008
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2022-09-19 18:48:47 +0900

    Merge (#63)
    * Feat : Zustand 설치 및 Counter 예제 (#27)
    
    * Chore : Redux 관련 삭제
    
    * Chore : zustand 설치 및 예제
    
    Co-authored-by: Inhoob <>
    
    * Feat: 회원가입 기능 (#28)
    
    * Feat: Add jwt login
    
    * Feat: Issue refresh token
    
    * feat: Complete refresh token and logout
    
    * Update server/build.gradle
    
    Co-authored-by: InhooPark <102350319+Inhoob@users.noreply.github.com>
    Co-authored-by: Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>

--- server/src/main/java/server/user/service/UserService.java
+++ server/src/main/java/server/user/service/UserService.java
@@ -1,8 +1,11 @@
 package server.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import server.user.entity.RefreshToken;
 import server.user.entity.User;
+import server.user.repository.RefreshTokenRepository;
 import server.user.repository.UserRepository;
 
 import java.util.Optional;
@@ -13,16 +16,26 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
     public User createUser(User user) throws Exception {
         verifyExistsEmail(user.getEmail());
         verifyExistsNickname(user.getNickname());
-
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setRole("ROLE_USER");
         return userRepository.save(user);
     }
 
     public User findUser(long userId) throws Exception {
         return findVerifiedUser(userId);
+    }
+
+    public void updateRefreshToken(String email, String refreshToken) {
+        RefreshToken refreshTokenEntity = RefreshToken.builder().email(email).refreshToken(refreshToken).build();
+        refreshTokenRepository.save(refreshTokenEntity);
     }
 
     public void updateUser(User user) throws Exception {
@@ -41,8 +54,7 @@ public class UserService {
 
     private User findVerifiedUser(long userId) throws Exception {
         Optional<User> user = userRepository.findById(userId);
-        User findUser = user.orElseThrow(() -> new Exception());
-        return findUser;
+        return user.orElseThrow(Exception::new);
     }
 
     private void verifyExistsEmail(String email) throws Exception {
