778c79a522e99271f82261d03c0127b94315da06
yeonkkk <tjddus0302@gmail.com>
2022-09-19 10:19:24 +0900

    nickname validation 변경

--- server/src/main/java/server/user/dto/UserPostDto.java
+++ server/src/main/java/server/user/dto/UserPostDto.java
@@ -21,7 +21,7 @@ public class UserPostDto {
     private String email;
 
     @NotBlank(message = "닉네임은 반드시 입력해야합니다.")
-    @Pattern(regexp = "(^[a-zA-Z]+(\\s[a-zA-Z]+)*$)",
+    @Pattern(regexp = "(^[a-zA-Z0-9가-힣]+(\\s[a-zA-Z0-9가-힣]+)*$)",
             message = "닉네임은 10자 이하의 영문으로 작성해야하며, 연속된 공백을 사용할 수 없습니다.")
     private String nickname;
 }
