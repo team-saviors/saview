e85512556e88e9cd29beb1dc96d3a9a9c7f48d77
Inhoob <>
2022-09-19 17:35:46 +0900

    Mini : axios post 수정

--- client/src/utils/axiosRequest.js
+++ client/src/utils/axiosRequest.js
@@ -1,9 +1,25 @@
 import client from './axiosInstance';
-async function postSignUp() {
-  const response = await client.post('/users');
-  console.log(response.data);
+async function postSignUp(data) {
+  try {
+    const res = await client.post('/users', data);
+    console.log(res);
+    alert('회원가입이 완료되었습니다!');
+  } catch (err) {
+    console.log('err : ', err.response);
+    alert('회원가입이 실패했습니다');
+  }
+}
+async function postSignIn(data) {
+  try {
+    const res = await client.post('/login', data);
+    console.log(res);
+  } catch (err) {
+    console.log('err : ', err.response);
+  }
 }
 async function getUser(userId) {
   const response = await client.get(`/users/${userId}`);
   console.log(response.data);
 }
+
+export { postSignUp, postSignIn };
