26f754b600dfa57131fef37e8c9531596fd100fe
Kyoungryun Kim <krkim1913@gmail.com>
2022-09-21 17:50:12 +0900

    fix: Modify questions to remain when deleting users

--- server/src/main/java/server/user/repository/UserRepository.java
+++ server/src/main/java/server/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package server.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.user.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByNickname(String nickname);
