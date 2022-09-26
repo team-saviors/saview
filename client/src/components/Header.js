dc153e2eca6cd78f4a0b92984250e43893aad0c0
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-09-26 21:10:22 +0900

    Feat/jwt token (#139)
    * [FE] Feat : jwt token 로직 구현중
    
    * [FE] Feat : access token 사용해서 api 요청 보내는것까지 가능하도록 구현.
    
    * [FE] Feat : 로그인시 jwt token cookie에 저장, 상태 변경 후 헤더 변경, 로그아웃 로직 구현
    
    * [FE] Feat : access-token 재발급 로직 구현중
    
    * 헤더 값 받아올 수 있도록 수정
    
    Co-authored-by: pjhyeok <vndepddl04@naver.com>

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,52 @@
 // import { Link } from '@mui/material';
-import { Box, Button } from '@mui/material';
-import styled from 'styled-components';
 import { SignInModal } from './SignInModal';
 import mainLogo from '../assets/images/mainlogo2.png';
 import { Link } from 'react-router-dom';
+import { getRefreshToken } from '../utils/cookies';
+import { loginStore } from '../store/store';
+
+const Header = () => {
+  // const refresh_token = getRefreshToken();
+  const { loginHandler, isLogin } = loginStore();
+  useEffect(() => {
+    loginHandler();
+    // if (refresh_token) {
+    //   loginHandler(true);
+    // } else {
+    //   loginHandler(false);
+    // }
+  }, []);
+
+  return (
+    <NavBar>
+      <LogoBox>
+        {/* <h1>SAVIEW</h1> */}
+        <Link to="/">
+          <img className="main_logo" alt="react" src={mainLogo}></img>
+        </Link>
+      </LogoBox>
+      <Loginbox>
+        <Link to="/questionpost">
+          <QuestionPostBtn disableRipple>새 질문 쓰기</QuestionPostBtn>
+        </Link>
+        {isLogin ? (
+          <>
+            <NoticeModal></NoticeModal>
+            <UserDropdown></UserDropdown>
+          </>
+        ) : (
+          <SignInModal></SignInModal>
+        )}
+      </Loginbox>
+    </NavBar>
+  );
+};
+
+import { Box, Button } from '@mui/material';
+import styled from 'styled-components';
+import NoticeModal from './NoticeModal';
+import UserDropdown from './UserDropdown';
+import { useEffect } from 'react';
 
 const NavBar = styled.header`
   /* width: 100%; */
@@ -38,23 +81,4 @@ const QuestionPostBtn = styled(Button)`
     appearance: none;
   }
 `;
-
-const Header = () => {
-  return (
-    <NavBar>
-      <LogoBox>
-        {/* <h1>SAVIEW</h1> */}
-        <Link to="/">
-          <img className="main_logo" alt="react" src={mainLogo}></img>
-        </Link>
-      </LogoBox>
-      <Loginbox>
-        <Link to="/questionpost">
-          <QuestionPostBtn disableRipple>새 질문 쓰기</QuestionPostBtn>
-        </Link>
-        <SignInModal></SignInModal>
-      </Loginbox>
-    </NavBar>
-  );
-};
 export default Header;
--- client/src/utils/cookies.js
+++ client/src/utils/cookies.js
@@ -1,5 +1,4 @@
 import { Cookies } from 'react-cookie';
-import { authClient } from './axiosInstance';
 const cookies = new Cookies();
 
 export const setAccessToken = (accessToken) => {
@@ -19,7 +18,7 @@ export const setRefreshToken = (refreshToken) => {
     sameSite: 'strict',
   });
 };
-export const getCookieToken = () => {
+export const getRefreshToken = () => {
   return cookies.get('refresh_token');
 };
 export const removeRefreshToken = () => {
