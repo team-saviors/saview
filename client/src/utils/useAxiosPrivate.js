971e09ca61cd21e6719ba6c3dd5ed2a08e6e17a5
박진혁 <93595307+pjhyeok@users.noreply.github.com>
2022-09-27 11:00:53 +0900

    Feat/jwt token (#142)
    * [FE] Feat : jwt token 로직 구현중
    
    * [FE] Feat : access token 사용해서 api 요청 보내는것까지 가능하도록 구현.
    
    * [FE] Feat : 로그인시 jwt token cookie에 저장, 상태 변경 후 헤더 변경, 로그아웃 로직 구현
    
    * [FE] Feat : access-token 재발급 로직 구현중
    
    * [FE] Feat : jwt 로그인,로그아웃 로직 완료
    
    * [FE]: fix 코드리뷰 후 수정

--- client/src/utils/useAxiosPrivate.js
+++ client/src/utils/useAxiosPrivate.js
@@ -1,20 +1,14 @@
-// import { authenticClient } from './axiosInstance';
-import { useEffect } from 'react';
 import { getAccessToken, getRefreshToken } from './cookies';
 import { getAccessWithRefresh } from './axiosRequest';
 import axios from 'axios';
 
 const authenticClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  // headers: {
-  //   Authorization: `${access_token?.access_token}`,
-  // },
 });
-// const useAxiosPrivate = () => {
+
 const refresh_token = getRefreshToken();
 const access_token = getAccessToken();
-//   useEffect(() => {
-// const requestIntercept =
+
 authenticClient.interceptors.request.use(
   (config) => {
     if (!config.headers['Authorization']) {
@@ -27,7 +21,6 @@ authenticClient.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-// const responseIntercept =
 authenticClient.interceptors.response.use(
   (response) => response,
   async (error) => {
@@ -43,13 +36,5 @@ authenticClient.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-// return () => {
-//   authenticClient.interceptors.request.eject(requestIntercept);
-//   authenticClient.interceptors.response.eject(responseIntercept);
-//     };
-//   }, [access_token, refresh_token]);
 
-//   return authenticClient;
-// };
 export default authenticClient;
-// export { useAxiosPrivate };
