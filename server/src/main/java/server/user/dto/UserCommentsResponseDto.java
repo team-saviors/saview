e3e10e070fb6ce69f40bf66effa4d11b0ccab1e2
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-09-28 14:20:01 +0900

    refactor: 유저페이지 작성글 반환값 수정 (#157)

--- server/src/main/java/server/user/dto/UserCommentsResponseDto.java
+++ server/src/main/java/server/user/dto/UserCommentsResponseDto.java
@@ -2,11 +2,11 @@ package server.user.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import server.comment.dto.CommentUserResponseDto;
+import server.response.AnswerCommentUserResponseDto;
 import server.response.MultiResponseDto;
 
 @Setter
 @Getter
 public class UserCommentsResponseDto {
-    private MultiResponseDto<CommentUserResponseDto> comments;
+    private MultiResponseDto<AnswerCommentUserResponseDto> myPosts;
 }
