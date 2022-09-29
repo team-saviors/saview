20acbbfdc342cf124f075779565e12dfe40566b8
Kyoungryun Kim <krkim1913@gmail.com>
2022-09-29 22:06:42 +0900

    fix: 서버 로그 타임존 제거 및 https세팅2

--- server/src/main/java/server/ServerApplication.java
+++ server/src/main/java/server/ServerApplication.java
@@ -6,17 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
-
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
