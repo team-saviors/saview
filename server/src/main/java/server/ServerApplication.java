8cfab2b218e6713d3c4ee065fecf9b126bb34655
Kyoungryun Kim <krkim1913@gmail.com>
2022-09-29 20:44:53 +0900

    fix: 서버 로그 타임존 수정 및 https세팅

--- server/src/main/java/server/ServerApplication.java
+++ server/src/main/java/server/ServerApplication.java
@@ -4,34 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedOrigins("*")
-//                        .allowedMethods(
-//                                HttpMethod.GET.name(),
-//                                HttpMethod.HEAD.name(),
-//                                HttpMethod.POST.name(),
-//                                HttpMethod.PUT.name(),
-//                                HttpMethod.DELETE.name());
-//            }
-//        };
-//    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
