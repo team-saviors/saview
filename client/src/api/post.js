efe36d13eb63df3da2dfb5e244cc0b54566d7afd
박진혁 <93595307+pjhyeok@users.noreply.github.com>
2022-10-01 02:29:05 +0900

    Feat/answer (#187)
    * [FE]  Refac : jwt-token 리팩토링, 코드수정, axosinterceptor 코드 수정, isLogin 상태 위치 변경 (#171)
    
    * [FE] Feat : Answer post 페이지 구현.
    
    * [FE] Refac : api 변경 적용, 로그아웃시 홈으로 redirect
    
    * [FE] Feat : 질문 답변 post 구현, axois interceptor 수정.
    
    * [FE] Fix : 로그인 후 post 요청시, 403에러 해결

---
+++ client/src/api/post.js
@@ -0,0 +1,15 @@
+import axiosInstance from '../utils/useAxiosPrivate';
+
+export async function postAnswer(quesionId, data) {
+  try {
+    const res = await axiosInstance.post(
+      `/questions/${quesionId}/answers`,
+      data
+    );
+    console.log(res);
+    alert('답변이 등록되었습니다.');
+  } catch (err) {
+    console.log(err);
+    alert(err);
+  }
+}
