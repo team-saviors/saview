072b6dc1afb152a7124a0bab17002353ec9b3647
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-10-05 09:49:55 +0900

    이메일 발송 기능 구현 (#208)
    * Feat: 이메일 발송 기본 코드 작성 및 테스트
    
    * Feat: 회원가입 메일 인증, 비밀번호 분실 시 임시 비밀번호 발급 기능 구현

---
+++ server/src/main/java/server/email/entity/EmailMessage.java
@@ -0,0 +1,15 @@
+package server.email.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class EmailMessage {
+
+    private String to;
+    private String subject;
+    private String message;
+}
