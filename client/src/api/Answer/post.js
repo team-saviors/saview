3ed24acce3d10f1baf59b7b18493a9cc4af55d43
Inhoob <>
2022-10-08 23:33:51 +0900

    Refactoring 예시

---
+++ client/src/api/Answer/post.js
@@ -0,0 +1,14 @@
+import axiosInstance from '../../utils/useAxiosPrivate';
+
+export async function postAnswer(quesionId, data) {
+  try {
+    const res = await axiosInstance.post(
+      `/questions/${quesionId}/answers`,
+      data
+    );
+    alert('답변이 등록되었습니다.');
+  } catch (err) {
+    console.log(err);
+    alert(err);
+  }
+}
--- client/src/api/post.js
+++ client/src/api/post.js
@@ -1,18 +1,5 @@
 import axiosInstance from '../utils/useAxiosPrivate';
 
-export async function postAnswer(quesionId, data) {
-  try {
-    const res = await axiosInstance.post(
-      `/questions/${quesionId}/answers`,
-      data
-    );
-    alert('답변이 등록되었습니다.');
-  } catch (err) {
-    console.log(err);
-    alert(err);
-  }
-}
-
 export async function postComment(answerId, data) {
   try {
     const res = await axiosInstance.post(`/answers/${answerId}/comments`, data);
