796fe9726d6c08a0c9e76bc89f396c43c231880e
InhooPark <102350319+Inhoob@users.noreply.github.com>
2022-10-11 01:03:36 +0900

    [FE] Refactor : QuestionCard 사이 마진 클릭 안되도록 수정 (#229)
    * SignInModal 어디서든 열릴수 있도록 refactoring
    
    * 로그인을 했을 때 메인페이지로 바로 가는것보단 그냥 현재 페이지에 있는것이 좋아보임
    
    * 로그인 안한 상태에서 질문/답변/댓글 작성시 로그인모달 띄우기
    
    * QuestionCard margin 사이 클릭 안되도록 변경
    
    Co-authored-by: Inhoob <>

--- client/src/pages/MainPage/QuestionCard.js
+++ client/src/pages/MainPage/QuestionCard.js
@@ -1,41 +1,39 @@
 import styled from 'styled-components';
 import Card from '@mui/material/Card';
 import { Box, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { RemoveRedEye } from '@mui/icons-material';
 import CommentIcon from '@mui/icons-material/Comment';
 
 const QuestionCard = (props) => {
   const { questionId, content, views, answerNum, subCategory } = props.question;
+  const navigate = useNavigate();
   return (
-    <Link to={`questions/${questionId}`}>
-      <CardItem
-        style={{ position: 'relative', borderRadius: '20px' }}
-        variant="outlined"
+    <CardItem
+      variant="outlined"
+      onClick={() => navigate(`questions/${questionId}`)}
+    >
+      <Button
+        variant="contained"
+        style={{
+          position: 'absolute',
+          left: '10px',
+          top: '10px',
+          borderRadius: '20px',
+        }}
       >
-        <Button
-          variant="contained"
-          style={{
-            position: 'absolute',
-            left: '10px',
-            top: '10px',
-            borderRadius: '20px',
-          }}
-        >
-          {subCategory}
-        </Button>
-        <Box style={{ margin: '10px' }}>
-          {/* outline 굵기 찾아봐야함 */}
-          <h1>{content}</h1>
-        </Box>
-        <Box style={{ poistion: 'relative', bottom: '5px' }}>
-          <RemoveRedEye style={{ top: '200px', color: 'gray' }} />
-          {views}
-          <CommentIcon style={{ color: 'gray' }}></CommentIcon>
-          {answerNum}
-        </Box>
-      </CardItem>
-    </Link>
+        {subCategory}
+      </Button>
+      <Box style={{ margin: '10px' }}>
+        <h1>{content}</h1>
+      </Box>
+      <Box style={{ poistion: 'relative', bottom: '5px' }}>
+        <RemoveRedEye style={{ top: '200px', color: 'gray' }} />
+        {views}
+        <CommentIcon style={{ color: 'gray' }}></CommentIcon>
+        {answerNum}
+      </Box>
+    </CardItem>
   );
 };
 const CardItem = styled(Card)`
@@ -47,5 +45,8 @@ const CardItem = styled(Card)`
   justify-content: center;
   margin-right: 40px;
   margin-bottom: 80px;
+  position: relative;
+  border-radius: 20px;
+  cursor: pointer;
 `;
 export default QuestionCard;
