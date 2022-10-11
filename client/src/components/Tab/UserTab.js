0cb2387f28288c2a36793e46376b9cd068044904
InhooPark <102350319+Inhoob@users.noreply.github.com>
2022-10-11 18:45:28 +0900

    회원탈퇴기능 추가 (#240)
    Co-authored-by: Inhoob <>

--- client/src/components/Tab/UserTab.js
+++ client/src/components/Tab/UserTab.js
@@ -1,13 +1,26 @@
 import Box from '@mui/material/Box';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { getUserId } from '../../utils/cookies';
+import AlertDialog from '../AlertDialog';
+import { deleteUser, postLogout } from '../../api/User';
+import { loginStore } from '../../store/store';
 export default function UserTab({ menu, setMenu }) {
+  const { loginHandler } = loginStore();
   const navigate = useNavigate();
   const params = useParams();
   const [active, setActive] = useState('회원정보');
-
+  const [open, setOpen] = useState(false);
+  const handleClose = async (e) => {
+    if (e.target.value === '삭제') {
+      await deleteUser();
+    }
+    setOpen(false);
+    await postLogout();
+    navigate('/');
+    await loginHandler();
+  };
   const handleClick = (e) => {
     if (e.target.id !== active) {
       setActive(e.target.id);
@@ -18,6 +31,9 @@ export default function UserTab({ menu, setMenu }) {
     }
     if (e.target.id === '내 계정') {
       navigate(`/users/${getUserId()}/modify`);
+    }
+    if (e.target.id === '회원탈퇴') {
+      setOpen(true);
     }
   };
 
@@ -31,15 +47,28 @@ export default function UserTab({ menu, setMenu }) {
             회원정보
           </Tab>
           {params.id === getUserId() ? (
-            <Tab
-              component={Link}
-              onClick={handleClick}
-              active={menu === '내 계정'}
-              id="내 계정"
-            >
-              내 계정
-            </Tab>
+            <>
+              <Tab
+                component={Link}
+                onClick={handleClick}
+                active={menu === '내 계정'}
+                id="내 계정"
+              >
+                내 계정
+              </Tab>
+              <Tab
+                onClick={(e) => handleClick(e)}
+                active={menu === '회원탈퇴'}
+                id="회원탈퇴"
+              >
+                회원탈퇴
+              </Tab>
+            </>
           ) : null}
+          <AlertDialog
+            open={open}
+            onClose={(e) => handleClose(e)}
+          ></AlertDialog>
         </Tabs>
       </Box>
     </Box>
