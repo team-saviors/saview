1fc672b9a6ae149577c5a39b9c7933928daa57de
박진혁 <93595307+pjhyeok@users.noreply.github.com>
2022-10-11 18:17:33 +0900

    페이지 이동시 최상단으로 이동하도록 구현. (#239)
    * [FE] Refactor : 로그아웃시 유저에게 alert 띄우기 구현, 로그인 없이 답변작성과 질문등록시 로직 수정.
    
    * [FE] Refactor : 로직수정.
    
    * [FE] Feat : 페이지 이동시 최상단으로 가도록 구현. 답변등록, 질문등록 로직 수정.
    
    * [FE] Refactor : 불필요한 로직 삭제.

--- client/src/api/Question/post.js
+++ client/src/api/Question/post.js
@@ -2,6 +2,7 @@ import axiosInstance from '../../utils/axiosInstance';
 export async function postQuestion(data) {
   try {
     const res = await axiosInstance.post('/questions', data);
+    alert('질문 작성이 완료되었습니다');
     return res;
   } catch (err) {
     return err;
