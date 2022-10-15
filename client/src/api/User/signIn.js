484257e64903e217b0ebddd70007af1896311bbd
Inhoob <>
2022-10-16 04:15:25 +0900

    postSignIn error 핸들링 개편 및 로그인실패시 password reset

--- client/src/api/User/signIn.js
+++ client/src/api/User/signIn.js
@@ -14,6 +14,6 @@ export async function postSignIn(data) {
     alert('로그인이 성공했습니다');
   } catch (err) {
     alert(err.response.data.message);
-    return err;
+    throw new Error('Login Error');
   }
 }
