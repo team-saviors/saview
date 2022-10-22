c5aa06e4292d6437d8dbc4ba50d1e2b83c1e041f
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2022-10-22 10:18:31 +0900

    fix: 탈퇴 유저 로그인 불가 처리 (#253)

--- server/src/main/java/server/jwt/handler/UserAuthenticationFailureHandler.java
+++ server/src/main/java/server/jwt/handler/UserAuthenticationFailureHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import server.exception.ErrorResponse;
@@ -18,8 +19,15 @@ public class UserAuthenticationFailureHandler implements AuthenticationFailureHa
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
         log.error("# Authentication failed: {}", exception.getMessage());
+        if (exception instanceof DisabledException) {
+            ErrorResponse errorResponse = ErrorResponse.of(ExceptionCode.QUIT_USER);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("utf-8");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            new ObjectMapper().writeValue(response.getOutputStream(), errorResponse);
+        }
+        else sendErrorResponse(response);
 
-        sendErrorResponse(response);
     }
 
     private void sendErrorResponse(HttpServletResponse response) throws IOException {
