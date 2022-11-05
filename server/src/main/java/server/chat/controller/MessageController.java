13e0cef0237f480b19ce52944d2b62f39c2bf4e7
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2022-11-05 14:49:39 +0900

    Feat/socket chat (#273)
    * 실시간 채팅 테스트
    
    * feat: 채팅창 ui 개선 (#267)
    
    * feat: 채팅룸 수정 초안 (#268)
    
    * Feat/socket chat 3 (#269)
    
    * feat: 채팅룸 수정 초안
    
    * feat: 방향 수정 및 룸 레포까지
    
    * fix: 채팅방 이미지 수정
    
    * 익명 유저 처리, 채팅방에서 나가는 기능 (#270)
    
    * feat: 채팅방에서 채팅 목록으로 나갈 수 있는 버튼 추가
    
    * feat: 익명의 유저 처리
    
    Co-authored-by: yeonkkk <tjddus0302@gmail.com>
    Co-authored-by: Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>

---
+++ server/src/main/java/server/chat/controller/MessageController.java
@@ -0,0 +1,28 @@
+package server.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.RestController;
+import server.chat.model.ChatMessage;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final SimpMessageSendingOperations sendingOperations;
+
+    // 실질적 경로: /app/chat/message
+    @MessageMapping("/chat/message")
+    public void enter(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender() + "님이 입장하였습니다.");
+        }
+        // /topic/chat/room/roomId 를 구독하는 클라이언트에게 메세지 전송
+        message.setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+        sendingOperations.convertAndSend("/topic/chat/room/" + message.getRoomId(), message);
+    }
+}
